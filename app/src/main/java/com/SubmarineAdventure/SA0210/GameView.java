5ca9fa182327fb317b7f2211e309ca0d63e5f37f
Temesgen Keife <64307949+TemuSoft@users.noreply.github.com>
2025-10-30 21:58:18 +0300

    Sate and bitmap initialization

--- app/src/main/java/com/SubmarineAdventure/SA0210/GameView.java
+++ app/src/main/java/com/SubmarineAdventure/SA0210/GameView.java
@@ -11,10 +11,13 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.view.View;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class GameView extends View{
+public class GameView extends View {
     private SharedPreferences sharedPreferences;
     private int screenX, screenY;
     private Resources resources;
@@ -27,7 +30,21 @@ public class GameView extends View{
     private int xSpeed, ySpeed;
     private Context context;
 
+    int[] subs = new int[]{R.drawable.decor_0, R.drawable.decor_1, R.drawable.decor_2, R.drawable.decor_3, R.drawable.decor_4};
+    int PEARL_VALUE = 0, TREASURE_VALUE = 1, COIN_VALUE = 2, SHIMMER_VALUE = 3, OXYGEN_VALUE = 4, JELLY_VALUE = 5, SHARK_VALUE = 6;
+    int scale_per_100 = 70;
     float health_remain = 100, oxygen_remain = 100;
+    Bitmap pearl, treasure, coin, shimmer, oxygen, jelly, shark, death, submarine;
+    int pr_w, pr_h, tr_w, tr_h;
+    int co_w, co_h, shi_w, shi_h;
+    int ox_w, ox_h, je_w, je_h;
+    int sha_w, sha_h, de_w, de_h;
+    int sub_x, sub_y, sub_w, sub_h;
+    int move_left, move_up;
+    int active_decor;
+    String image_uri;
+    ArrayList<Bitmap> bitmaps = new ArrayList<>();
+    ArrayList<ArrayList<Integer>> game_data = new ArrayList<>();
 
     public GameView(Context mContext, int scX, int scY, Resources res, int level_amount) {
         super(mContext);
@@ -37,8 +54,84 @@ public class GameView extends View{
         context = mContext;
         random = new Random();
 
+        sharedPreferences = context.getSharedPreferences("arineA5lA0210", context.MODE_PRIVATE);
+        active_decor = sharedPreferences.getInt("active_decor", 0);
+        image_uri = sharedPreferences.getString("image_uri", "");
 
+        initialize_data(res);
         setSpeed();
+    }
+
+    private void initialize_data(Resources res) {
+        pearl = BitmapFactory.decodeResource(res, R.drawable.pearl);
+        treasure = BitmapFactory.decodeResource(res, R.drawable.treasure);
+        coin = BitmapFactory.decodeResource(res, R.drawable.coin);
+        shimmer = BitmapFactory.decodeResource(res, R.drawable.shimmer);
+        oxygen = BitmapFactory.decodeResource(res, R.drawable.oxygen);
+        jelly = BitmapFactory.decodeResource(res, R.drawable.jelly);
+        shark = BitmapFactory.decodeResource(res, R.drawable.shark);
+        death = BitmapFactory.decodeResource(res, R.drawable.death);
+        submarine = BitmapFactory.decodeResource(res, subs[active_decor]);
+        if (active_decor == 4) {
+            try {
+                URL url = new URL(image_uri);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                submarine = BitmapFactory.decodeStream(input);
+            } catch (Exception e) {
+
+            }
+        }
+
+        pr_w = pearl.getWidth() * scale_per_100 / 100;
+        pr_h = pearl.getHeight() * scale_per_100 / 100;
+
+        tr_w = treasure.getWidth() * scale_per_100 / 100;
+        tr_h = treasure.getWidth() * scale_per_100 / 100;
+
+        co_w = coin.getWidth() * scale_per_100 / 100;
+        co_h = coin.getWidth() * scale_per_100 / 100;
+
+        shi_w = shimmer.getWidth() * scale_per_100 / 100;
+        shi_h = shimmer.getWidth() * scale_per_100 / 100;
+
+        ox_w = oxygen.getWidth() * scale_per_100 / 100;
+        ox_h = oxygen.getWidth() * scale_per_100 / 100;
+
+        je_w = jelly.getWidth() * scale_per_100 / 100;
+        je_h = jelly.getWidth() * scale_per_100 / 100;
+
+        sha_w = shark.getWidth() * scale_per_100 / 100;
+        sha_h = shark.getWidth() * scale_per_100 / 100;
+
+        de_w = death.getWidth();
+        de_h = death.getWidth();
+
+        sub_w = submarine.getWidth() * scale_per_100 / 100;
+        sub_h = submarine.getWidth() * scale_per_100 / 100;
+
+        sub_x = random.nextInt(screenX - sub_w * 5) + sub_w * 2;
+        sub_y = random.nextInt(screenX - sub_h * 7) + sub_h * 2;
+
+        pearl = Bitmap.createScaledBitmap(pearl, pr_w, pr_h, false);
+        treasure = Bitmap.createScaledBitmap(treasure, tr_w, tr_h, false);
+        coin = Bitmap.createScaledBitmap(coin, co_w, co_h, false);
+        shimmer = Bitmap.createScaledBitmap(shimmer, shi_w, shi_h, false);
+        oxygen = Bitmap.createScaledBitmap(oxygen, ox_w, ox_h, false);
+        jelly = Bitmap.createScaledBitmap(jelly, je_w, je_h, false);
+        shark = Bitmap.createScaledBitmap(shark, sha_w, sha_h, false);
+        death = Bitmap.createScaledBitmap(death, de_w, de_h, false);
+        submarine = Bitmap.createScaledBitmap(submarine, sub_w, sub_h, false);
+
+        bitmaps.add(pearl);
+        bitmaps.add(treasure);
+        bitmaps.add(coin);
+        bitmaps.add(shimmer);
+        bitmaps.add(oxygen);
+        bitmaps.add(jelly);
+        bitmaps.add(shark);
     }
 
     public void onDraw(Canvas canvas) {
@@ -47,6 +140,10 @@ public class GameView extends View{
         canvas.drawColor(Color.TRANSPARENT);
 
 
+        canvas.save();
+        canvas.scale(move_left, 1, sub_x + sub_w / 2, sub_y + sub_h / 2);
+        canvas.drawBitmap(submarine, sub_x, sub_y, paint);
+        canvas.restore();
 
     }
 
@@ -58,5 +155,12 @@ public class GameView extends View{
     public void update() {
 
         invalidate();
+    }
+
+    public Rect getSubmarineCollision(){
+        int mw = sub_w / 6;
+        int mh = sub_h / 6;
+
+        return new Rect(sub_x + mw, sub_y + mh, sub_x + sub_w - mw, sub_y + sub_h - mh);
     }
 }
--- app/src/main/java/com/SubmarineAdventure/SA0210/StatsActivity.java
+++ app/src/main/java/com/SubmarineAdventure/SA0210/StatsActivity.java
@@ -2,8 +2,8 @@ package com.SubmarineAdventure.SA0210;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,7 +12,28 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class StatsActivity extends AppCompatActivity {
+    private LinearLayout collect_artifact, dive;
+    private TextView collect_artifact_status, dive_status;
+
+    private LineChart lineChart;
+    private BarChart barChart;
+
     private ImageView back;
     private TextView coin;
     private Button reset_progress;
@@ -22,6 +43,9 @@ public class StatsActivity extends AppCompatActivity {
     private Intent intent;
     private String lang;
     private int available_coin;
+    private boolean dive_500m_achieved;
+    private int total_artifact_collected;
+    private int distance_traveled, treasure_amount, pearl_amount, coin_amount, shimmer_amount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +61,34 @@ public class StatsActivity extends AppCompatActivity {
         onVibrating = sharedPreferences.getBoolean("onVibrating", false);
         lang = sharedPreferences.getString("lang", "");
         available_coin = sharedPreferences.getInt("available_coin", 0);
+        dive_500m_achieved = sharedPreferences.getBoolean("dive_500m_achieved", false);
+        total_artifact_collected = sharedPreferences.getInt("total_artifact_collected", 0);
+        distance_traveled = sharedPreferences.getInt("distance_traveled", 0);
+        treasure_amount = sharedPreferences.getInt("treasure_amount", 0);
+        pearl_amount = sharedPreferences.getInt("pearl_amount", 0);
+        coin_amount = sharedPreferences.getInt("coin_amount", 0);
+        shimmer_amount = sharedPreferences.getInt("shimmer_amount", 0);
+
+        treasure_amount = new Random().nextInt(50);
+        pearl_amount = new Random().nextInt(50);
+        coin_amount = new Random().nextInt(50);
 
         setContentView(R.layout.activity_stats);
 
         back = findViewById(R.id.back);
         coin = findViewById(R.id.coin);
         reset_progress = findViewById(R.id.reset_progress);
+
+        dive = findViewById(R.id.dive);
+        collect_artifact = findViewById(R.id.collect_artifact);
+        dive_status = findViewById(R.id.dive_status);
+        collect_artifact_status = findViewById(R.id.collect_artifact_status);
+
+        lineChart = findViewById(R.id.lineChart);
+        barChart = findViewById(R.id.barChart);
+
+        setupLineChart();
+        setupBarChart();
 
         back.setOnClickListener(view -> {
             Player.button(soundMute);
@@ -52,25 +98,129 @@ public class StatsActivity extends AppCompatActivity {
         reset_progress.setOnClickListener(View -> {
             Player.button(soundMute);
 
+            editor.putInt("distance_traveled", 0);
+            editor.putInt("treasure_amount", 0);
+            editor.putInt("pearl_amount", 0);
+            editor.putInt("coin_amount", 0);
+            editor.putInt("shimmer_amount", 0);
 
+            editor.putBoolean("dive_500m_achieved", false);
+            editor.putInt("total_artifact_collected", 0);
+            editor.apply();
+
+            intent = new Intent(StatsActivity.this, StatsActivity.class);
+            startActivity(intent);
+            overridePendingTransition(0, 0);
+            finish();
         });
+
+        if (dive_500m_achieved) {
+            dive.setBackgroundResource(R.drawable.green_rect);
+            dive_status.setText(getResources().getString(R.string.collected));
+        } else {
+            dive.setBackgroundResource(R.drawable.light_blue_rect);
+            dive_status.setText(getResources().getString(R.string.in_progress));
+        }
+
+        if (total_artifact_collected >= 100) {
+            collect_artifact.setBackgroundResource(R.drawable.green_rect);
+            collect_artifact_status.setText(getResources().getString(R.string.collected));
+        } else {
+            collect_artifact.setBackgroundResource(R.drawable.light_blue_rect);
+            collect_artifact_status.setText(getResources().getString(R.string.in_progress));
+        }
+
 
         coin.setText(available_coin + "");
     }
+    private void setupLineChart() {
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(0, 0));
+        entries.add(new Entry(1, 10));
+        entries.add(new Entry(2, 40));
+        entries.add(new Entry(3, 70));
+        entries.add(new Entry(4, 100));
+        entries.add(new Entry(5, 50));
+        entries.add(new Entry(6, 20));
+        entries.add(new Entry(7, 40));
+
+        int primary = getResources().getColor(R.color.primary);
+        LineDataSet dataSet = new LineDataSet(entries, "");
+        dataSet.setColor(primary);
+        dataSet.setLineWidth(3f);
+        dataSet.setDrawCircles(false);
+        dataSet.setDrawValues(false);
+
+        LineData data = new LineData(dataSet);
+        lineChart.setData(data);
+
+        lineChart.getDescription().setEnabled(false);
+        lineChart.getLegend().setEnabled(false);
+        lineChart.setTouchEnabled(false);
+        lineChart.setDrawBorders(false);
+
+        lineChart.getXAxis().setEnabled(false);
+
+        lineChart.getAxisRight().setEnabled(false);
+        YAxis leftAxis = lineChart.getAxisLeft();
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setGridColor(getResources().getColor(R.color.light_blue));
+        leftAxis.setGridLineWidth(0.5f);
+        leftAxis.setTextColor(primary);
+        leftAxis.setAxisLineColor(primary);
+
+        lineChart.invalidate();
+    }
+
+    private void setupBarChart() {
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0, treasure_amount));
+        entries.add(new BarEntry(1, pearl_amount));
+        entries.add(new BarEntry(2, coin_amount));
+
+        int primary = getResources().getColor(R.color.primary);
+        BarDataSet barDataSet = new BarDataSet(entries, "");
+        barDataSet.setColor(primary);
+        barDataSet.setDrawValues(false);
+
+        BarData data = new BarData(barDataSet);
+        data.setBarWidth(0.4f);
+
+        barChart.setData(data);
+        barChart.getDescription().setEnabled(false);
+        barChart.getLegend().setEnabled(false);
+        barChart.setTouchEnabled(false);
+
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setEnabled(false);
+
+        YAxis leftAxis = barChart.getAxisLeft();
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setTextColor(primary);
+        leftAxis.setAxisLineColor(primary);
+        leftAxis.setAxisMinimum(0f);
+
+        barChart.getAxisRight().setEnabled(false);
+
+        barChart.setFitBars(false);
+
+        barChart.setViewPortOffsets(60f, 20f, 60f, 20f);
+
+        barChart.invalidate();
+    }
+
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (!isMute)
-            Player.all_screens.pause();
+        if (!isMute) Player.all_screens.pause();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         isMute = sharedPreferences.getBoolean("isMute", false);
-        if (!isMute)
-            Player.all_screens.start();
+        if (!isMute) Player.all_screens.start();
     }
 
     @Override
